4afe51c4f5be857d0c5222a29b2947e34189ae20
MilenaBelica <milena.belica@outlook.com>
2025-10-21 15:14:30 +0200

    Dodaj pliki projektów.

---
+++ Lab 3/Lab 3.cpp
@@ -0,0 +1,11 @@
+﻿// Lab 3.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
